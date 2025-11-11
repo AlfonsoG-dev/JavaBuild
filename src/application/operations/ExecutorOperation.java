0d61d18f84e1b3b49a8575e0c87ee368fe2d1e54
AlfonsoG-dev <alfgomachi@gmail.com>
2025-11-11 17:30:07 -0500

    refactor: shutdown executor gracefully

--- src/application/operations/ExecutorOperation.java
+++ src/application/operations/ExecutorOperation.java
@@ -1,11 +1,12 @@
 package application.operations;
 
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.Callable;
-import java.util.concurrent.CancellationException;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.CancellationException;
 
 public class ExecutorOperation {
     
@@ -18,14 +19,27 @@ public class ExecutorOperation {
      */
     public <T> T executeConcurrentCallableList(Callable<T> task) {
         T value = null;
-        try (ExecutorService ex = Executors.newCachedThreadPool()) {
-            Future<T> result = ex.submit(task);
-            if(result.state() == Future.State.RUNNING) {
+        ExecutorService executor = Executors.newCachedThreadPool();
+        try {
+            Future<T> result = executor.submit(task);
+            if(!result.isDone()) {
                 System.out.println("[Info] Waiting for data...");
             }
             value = result.get();
+            if(result.isDone()) {
+                System.out.println("[Info] Data is ready...");
+            }
         } catch (ExecutionException | InterruptedException | CancellationException e) {
             e.printStackTrace();
+        } finally {
+            executor.shutdown();
+            try {
+                if(!executor.awaitTermination(5, TimeUnit.SECONDS)) {
+                    executor.shutdownNow();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
         return value;
     }
