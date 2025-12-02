9fd392c18717e286608a65da163dc17f70c2a025
AlfonsoG-dev <alfgomachi@gmail.com>
2025-12-01 22:12:58 -0500

    feat: implement command line operations

---
+++ src/application/JavaBuild.java
@@ -0,0 +1,22 @@
+package application;
+
+import application.operations.Operation;
+
+class JavaBuild {
+    public static void main(String[] args) {
+        Operation op = new Operation(args);
+        op.startUpConfig();
+        for(String a: args) {
+            switch(a) {
+                case "--compile":
+                    op.compileOperation();
+                    break;
+                case "--h":
+                    System.console().printf("%s%n", "Use --h for help");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
