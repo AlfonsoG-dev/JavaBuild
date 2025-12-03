6455b653c74db76489835da0e7f741c0c1efe068
AlfonsoG-dev <alfgomachi@gmail.com>
2025-12-02 22:11:06 -0500

    refactor: use CommanModel interface

--- src/application/builders/LibBuilder.java
+++ src/application/builders/LibBuilder.java
@@ -2,11 +2,22 @@ package application.builders;
 
 import java.io.File;
 
-public record LibBuilder(String root, JarBuilder jarBuilder) {
-    private static final String DEFAULT_EXTRACT_PATH = "extractionFiles";
-    private static final String DEFAULT_LIB_PATH = "lib";
+import application.models.CommandModel;
+import application.operations.FileOperation;
+
+public record LibBuilder(String root, FileOperation fileOperation) implements CommandModel  {
     private static final String FILE_EXTENSION = ".jar";
     private static final String CONSOLE_FORMAT = "%s%n";
+
+    @Override
+    public FileOperation getFileOperation() {
+        return fileOperation;
+    }
+
+    @Override
+    public String getRoot() {
+        return root;
+    }
 
     /**
      * Extract the content of the .jar lib dependency into the extractionFiles directory.
@@ -15,26 +26,28 @@ public record LibBuilder(String root, JarBuilder jarBuilder) {
      * @param includeLib - to include or not the .jar dependency.
      * @return a command for each one of the extract files separated by ";".
      */
-    public String getExtractCommand(String includeLib) {
+    public String getCommand(String sourcePath, String classPath, String flags, String includeLib) {
         // TODO: test this.
         if(!includeLib.equals("include")) return "";
         StringBuilder command = new StringBuilder();
 
-        File[] libFiles = new File(DEFAULT_LIB_PATH).listFiles();
-        if(libFiles == null || libFiles.length == 0) return "";
+        String[] libFiles = preparedLibFiles(sourcePath).split(";");
+        if(libFiles.length == 0) return "";
+
+        for(String l: libFiles) {
+            //TODO: copy files to extractionFiles
+            System.console().printf(CONSOLE_FORMAT, l);
+        }
 
         // copy files from lib to extractionFiles
-        for(File l: libFiles) {
-            for(File f: l.listFiles()) {
-                System.console().printf(CONSOLE_FORMAT, f.toPath().normalize().toString());
-            }
-        }
-        File[] extractFiles = new File(DEFAULT_EXTRACT_PATH).listFiles();
+        File[] extractFiles = new File(classPath).listFiles();
         if(extractFiles == null || extractFiles.length == 0) return "";
 
         // extract .jar files.
         for(File f: extractFiles) {
             command.append(String.format("cd %s && ", f.toPath().normalize().toString()));
+            // FIXME: validate that the file and its parent share the same name.
+            // assuming that the .jar file has the same name as its parent folder.
             command.append(String.format("jar -xf %s%s && ", f.getName(), FILE_EXTENSION));
             command.append(String.format("rm -r %s%s%n", f.getName(), FILE_EXTENSION));
         }
