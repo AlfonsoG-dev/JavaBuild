e5155fe92ebfc0d6f85a2fc1a34401d4aba407c3
AlfonsoG-dev <alfgomachi@gmail.com>
2025-12-08 12:03:49 -0500

    build: add debugg capabilities

--- build.ps1
+++ build.ps1
@@ -1,5 +1,5 @@
 $SourceFiles = "src\application\*.java src\application\builders\*.java src\application\models\*.java src\application\operations\*.java src\application\utils\*.java"
-$Compile = "javac -d bin -Werror $SourceFiles"
+$Compile = "javac -d bin -Werror -g $SourceFiles"
 $CreateJar = "jar -cfm JavaBuild.jar Manifesto.txt -C .\bin\ ."
 $Run = "java -jar JavaBuild.jar"
 Invoke-Expression ($Compile + " && " + $CreateJar + " && " + $Run)
