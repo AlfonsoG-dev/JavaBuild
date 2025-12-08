a4fadd4437c63dcdbc894e90c0c80e3dd470c600
AlfonsoG-dev <alfgomachi@gmail.com>
2025-12-08 12:05:05 -0500

    feat: implement process execution class

---
+++ src/application/operations/ProcessOperation.java
@@ -0,0 +1,11 @@
+package application.operations;
+
+import java.io.BufferedReader;
+import java.io.Console;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class ProcessOperation {
+}
