60f75c42cd14ace3ced2cf80792efc3fced1e41e
AlfonsoG-dev <alfgomachi@gmail.com>
2025-12-08 12:06:03 -0500

    feat: add local static variables

--- src/application/operations/ProcessOperation.java
+++ src/application/operations/ProcessOperation.java
@@ -8,4 +8,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public class ProcessOperation {
+    private static final String LOCAL_PATH = "." + File.separator;
+    private static final boolean OS_IS_WINDOWS = System.getProperty("os.name").toLowerCase().contains("windows");
+    private static final String CONSOLE_FORMAT = "[%s] %s%n";
+    private static Console console = System.console();
 }
